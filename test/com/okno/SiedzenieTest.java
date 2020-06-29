ccb2834f3f3e63bf29435542a7fe8a8fff52c6e7
mgdmrk <mgdmrk@users.noreply.github.com>
2020-06-29 15:27:31 +0200

    Update SiedzenieTest.java

--- test/com/okno/SiedzenieTest.java
+++ test/com/okno/SiedzenieTest.java
@@ -1,12 +1,26 @@
 package com.okno;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import javax.swing.*;
 
 import static org.junit.Assert.*;
 
 public class SiedzenieTest {
+    private Siedzenie siedzenie;
+
+    @Before
+    public void setUp() {
+        siedzenie = new Siedzenie(1, 3, "A3", State.WOLNE);
+        ImageIcon iWolne = new ImageIcon("Coś tam\\Nowe Grafiki\\wolne.png");
+    }
+
 
     @Test
-    public void setState() {
+    public void isWolne() {
+
+        assertEquals(State.WOLNE, siedzenie.getState());
     }
 }
+    
