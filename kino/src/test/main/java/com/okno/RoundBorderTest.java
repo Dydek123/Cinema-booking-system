fdb89d07fb3af10437474cadf9d6fb83e8282903
smasiek <48649517+smasiek@users.noreply.github.com>
2020-06-30 14:19:29 +0200

    ponowne dodanie testow Magdy

---
+++ kino/src/test/main/java/com/okno/RoundBorderTest.java
@@ -0,0 +1,26 @@
+package main.java.com.okno;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class RoundBorderTest {
+    private RoundBorder border;
+
+    @Before
+    public void  setUp(){
+        border = new RoundBorder(4);
+    }
+
+    @Test
+    public void isBorderOpaque() {
+        boolean isTrue = true;
+        try{
+            border.isBorderOpaque();
+        }catch(Exception exception) {
+            isTrue = false;
+        }
+        assertTrue(isTrue);
+
+    }
+}
